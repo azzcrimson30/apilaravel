3d169a3091e601b18c3f954eb5cabd949927a54a
azzcrimson30 <asnawihashim30@gmail.com>
2022-01-27 11:11:10 +0800

    initial psh

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Passport\Passport;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'App\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+        Passport::routes();
+
+        $startTime = date("Y-m-d H:i:s");
+        $endTime = date("Y-m-d H:i:s", strtotime('+1 day +1 hour +1 minutes +1 seconds',
+            strtotime($startTime)));
+        $expTime = \DateTime::createFromFormat("Y-m-d H:i:s", $endTime);
+        Passport::tokensExpireIn($expTime);
+    }
+}
